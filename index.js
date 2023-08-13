f69415ed9bcdec74ff0805eb8348593588139f8c
Obayuwana22 <successobayuwana@gmail.com>
2023-08-13 13:42:44 +0100

    New javacript codes

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
-let newVariable = "none";
+let submit = document.querySelector("#submit").addEventListener("click", run);
+let ratingNumbers = document.querySelectorAll(".listItem");
+let rating = document.querySelector("#rating");
+let appreciation = document.querySelector("#appreciation");
+let selectedValue = document.querySelector("#selectedValue");
 
-document.querySelector("#submit").addEventListener("click", show);
-document.querySelector("#selectOne").addEventListener("click", runOne);
-document.querySelector("#selectTwo").addEventListener("click", runTwo);
-document.querySelector("#selectThree").addEventListener("click", runThree);
-document.querySelector("#selectFour").addEventListener("click", runFour);
-document.querySelector("#selectFive").addEventListener("click", runFive);
-
-function show() {
-  document.querySelector("#rating").classList.add("hidden");
-  document.querySelector("#appreciation").classList.toggle("hidden");
+function run() {
+  rating.classList.add("hidden");
+  appreciation.classList.toggle("hidden");
 }
 
-function runOne() {
-  newVariable = 1;
-  document.querySelector("#selectOne").style.background = "hsl(216, 12%, 54%)";
-  document.querySelector("#selectOne").style.color = "white";
-  document.querySelector("#selectedValue").innerText = 1;
+function removeSelectedClass() {
+  ratingNumbers.forEach(function (rate) {
+    if (rate.classList.contains("selected")) {
+      rate.classList.remove("selected");
+    }
+  });
 }
 
-function runTwo() {
-  newVariable = 2;
-  document.querySelector("#selectTwo").style.background = "hsl(216, 12%, 54%)";
-  document.querySelector("#selectTwo").style.color = "white";
-  document.querySelector("#selectedValue").innerText = 2;
-}
-
-function runThree() {
-  document.querySelector("#selectThree").style.background =
-    "hsl(216, 12%, 54%)";
-  document.querySelector("#selectThree").style.color = "white";
-  document.querySelector("#selectedValue").innerText = 3;
-}
-
-function runFour() {
-  document.querySelector("#selectFour").style.background = "hsl(216, 12%, 54%)";
-  document.querySelector("#selectFour").style.color = "white";
-  document.querySelector("#selectedValue").innerText = 4;
-}
-
-function runFive() {
-  document.querySelector("#selectFive").style.background = "hsl(216, 12%, 54%)";
-  document.querySelector("#selectFive").style.color = "white";
-  document.querySelector("#selectedValue").innerText = 5;
+function toggleSelection(element) {
+  removeSelectedClass();
+  element.classList.toggle("selected");
 }
